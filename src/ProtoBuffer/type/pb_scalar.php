1d265b57fc4acdcf82118cbfb62c27c645729ed0
fengchenghao <fengchenghao@tatagou.cc>
2017-10-17 00:49:55 +0800

    add src

---
+++ src/ProtoBuffer/type/pb_scalar.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Aliyun\OTS\ProtoBuffer;
+/**
+ * @author Nikolai Kordulla
+ */
+class PBScalar extends PBMessage
+{
+	/**
+	 * Set scalar value
+	 */
+	public function set_value($value)
+	{	
+		$this->value = $value;	
+	}
+
+	/**
+	 * Get the scalar value
+	 */
+	public function get_value()
+	{
+		return $this->value;
+	}
+}
+?>
